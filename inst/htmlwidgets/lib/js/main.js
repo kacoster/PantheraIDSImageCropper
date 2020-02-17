5d49e180ad230a5420bfa0b8e233b4f9a2694e8d
dmdeveloper <kudatawira@gmail.com>
2020-02-18 01:41:17 +0200

    Update main.js

--- inst/htmlwidgets/lib/js/main.js
+++ inst/htmlwidgets/lib/js/main.js
@@ -287,7 +287,8 @@ $(function () {
                 reader.onload = function (e) {
                     let img = $("<img />");
                     img.attr("style", "max-height:160px;max-width: 120px;width: auto;height:auto;");
-                    let syl = 'max-height:160px;max-width: 120px;width: auto;height:auto; background-color: yellow;';
+                    let imgsyl = 'max-height:160px;max-width: 120px;width: auto;height:auto; background-color: yellow;';
+                    let ulstyle = 'background-color: yellow;';
                     let cl = 'border';
                     img.attr("src", e.target.result);
                     //img.attr("filename",file1.name);
@@ -296,7 +297,7 @@ $(function () {
                     img.attr("class","border");
                     let liID = (imagefile.name).slice(0, (imagefile.name).indexOf("."));
                     //dvPreview.append(img);
-                    let myli = '<li id="' + liID + '" ><img id="' + imagefile.type + '" style="' + syl + '"  class="' + cl + '" src="' + e.target.result + '"  alt="' + imagefile.name + '" /> </li>';
+                    let myli = '<li id="' + liID + '" style="' + ulstyle + '" ><img id="' + imagefile.type + '" style="' + imgsyl + '"  class="' + cl + '" src="' + e.target.result + '"  alt="' + imagefile.name + '" /> </li>';
                     dvPreview.append(myli);
                     //uploadedImageName = file1.name;
                     //uploadedImageType = file1.type;
