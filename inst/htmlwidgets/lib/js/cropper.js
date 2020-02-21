4cda469bb026ee73121816d92ba68010fa5b8504
dmdeveloper <kudatawira@gmail.com>
2020-02-21 09:54:14 +0200

    Update cropper.js

--- inst/htmlwidgets/lib/js/cropper.js
+++ inst/htmlwidgets/lib/js/cropper.js
@@ -1454,7 +1454,7 @@
         }
   
         this.cropBoxData = cropBoxData;
-        this.limitCropBox(true, true); // Initialize auto crop area
+        //this.limitCropBox(true, true); // Initialize auto crop area
   
         cropBoxData.width = Math.min(Math.max(cropBoxData.width, cropBoxData.minWidth), cropBoxData.maxWidth);
         cropBoxData.height = Math.min(Math.max(cropBoxData.height, cropBoxData.minHeight), cropBoxData.maxHeight); // The width/height of auto crop area must large than "minWidth/Height"
@@ -1510,11 +1510,11 @@
           cropBoxData.maxWidth = maxCropBoxWidth;
           cropBoxData.maxHeight = maxCropBoxHeight;
           console.log("initCropBox");
-          $('div.cropper-crop-box').css({
-            "width": "88.px",
-            "height": "49px",
-            "transform": "translateX(7%) translateY(426.875px)"
-          });
+          //$('div.cropper-crop-box').css({
+          //  "width": "88.px",
+          //  "height": "49px",
+          //  "transform": "translateX(7%) translateY(426.875px)"
+          //});
         }
   
         if (positionLimited) {
