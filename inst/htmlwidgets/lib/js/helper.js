a3efb11e92bf483246a517d0c2b84498eba491eb
dmdeveloper <kudatawira@gmail.com>
2020-02-27 08:16:33 +0200

    Update helper.js

--- inst/htmlwidgets/lib/js/helper.js
+++ inst/htmlwidgets/lib/js/helper.js
@@ -68,24 +68,6 @@ $(document).ready(function(){
   );
 
   $('#btn-grp-5').append(
-    '<label class="btn btn-primary">'+
-      '<input type="radio" class="sr-only" id="aspectRatio0" name="aspectRatio" value="1.7777777777777777">'+
-      '<span class="docs-tooltip" data-toggle="tooltip" data-animation="false" title="aspectRatio: 16 / 9">'+
-      '16:9'+
-      '</span>'+
-      '</label>'+
-      '<label class="btn btn-primary">'+
-      '<input type="radio" class="sr-only" id="aspectRatio1" name="aspectRatio" value="1.3333333333333333">'+
-      '<span class="docs-tooltip" data-toggle="tooltip" data-animation="false" title="aspectRatio: 4 / 3">'+
-      '4:3'+
-      '</span>'+
-      '</label>'+
-      '<label class="btn btn-primary">'+
-      '<input type="radio" class="sr-only" id="aspectRatio3" name="aspectRatio" value="0.6666666666666666">'+
-      '<span class="docs-tooltip" data-toggle="tooltip" data-animation="false" title="aspectRatio: 2 / 3">'+
-      '2:3' +
-      '</span>'+
-      '</label>'+
       '<label class="btn btn-primary">'+
       '<input type="radio" class="sr-only" id="aspectRatio4" name="aspectRatio" value="NaN">'+
       '<span class="docs-tooltip" data-toggle="tooltip" data-animation="false" title="aspectRatio: NaN">'+
