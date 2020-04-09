dc6bcefff23b1ed20b10bfd78e3d3e36937daf2d
dmdeveloper <kudatawira@gmail.com>
2020-04-09 19:26:54 +0200

    Update sngl_img_viewer.js

--- inst/htmlwidgets/sngl_img_viewer.js
+++ inst/htmlwidgets/sngl_img_viewer.js
@@ -20,7 +20,7 @@ HTMLWidgets.widget({
         Shiny.addCustomMessageHandler("spcs_idntfctn_extrt_id_button_rf_1",
                 function(mesg) {
                    console.log("Handler spcs_idntfctn_extrt_id_button_rf_1");
-                  let spcs_idntfctns_prmry_img = new ViewerComponent(x.targetId,x.filepath);
+                  let spcs_idntfctns_prmry_img = new ViewerComponent("spcs_idntfctn_id_rf_1","img_idntfctn_prmry.csv");
                   spcs_idntfctns_prmry_img.fetchServerData();
                   //resetProps();
                 }
@@ -29,7 +29,7 @@ HTMLWidgets.widget({
          Shiny.addCustomMessageHandler("spcs_idntfctn_extrt_id_button_rf_2",
                 function(mesg) {
                   console.log("Handler spcs_idntfctn_extrt_id_button_rf_2");
-                  let spcs_idntfctns_scndry_img = new ViewerComponent(x.targetId,x.filepath);
+                  let spcs_idntfctns_scndry_img = new ViewerComponent("spcs_idntfctn_id_rf_2","img_idntfctn_scndry.csv");
                   spcs_idntfctns_scndry_img.fetchServerData();
                   // nextPrevClicked("1");
                   //resetProps(); spcs_idntfctns_prmry_img
