ec61608d7e88ec36928fff52b99bfa29f0cac666
dmdeveloper <kudatawira@gmail.com>
2020-04-14 13:40:44 +0200

    Update sngl_img_vwr.js

--- inst/htmlwidgets/lib/js/sngl_img_vwr.js
+++ inst/htmlwidgets/lib/js/sngl_img_vwr.js
@@ -7,7 +7,7 @@
  *
  * Date: 2019-10-04T04:33:48.372Z
  */
-console.log("In cp.js");
+console.log("In cp.js v 1");
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -259,6 +259,25 @@ console.log("In cp.js");
               '<div class="cropper-canvas"></div>' + 
       '</div>' + 
       '<div class="cropper-drag-box"></div>' + 
+      '<div class="cropper-crop-box">' + 
+            '<span class="cropper-view-box"></span>' + 
+            '<span class="cropper-dashed dashed-h"></span>' +
+            '<span class="cropper-dashed dashed-v"></span>' + 
+            '<span class="cropper-center"></span>' + 
+            '<span class="cropper-face"></span>' + 
+            '<span class="cropper-line line-e" data-cropper-action="e"></span>' + 
+            '<span class="cropper-line line-n" data-cropper-action="n"></span>' + 
+            '<span class="cropper-line line-w" data-cropper-action="w"></span>' + 
+            '<span class="cropper-line line-s" data-cropper-action="s"></span>' + 
+            '<span class="cropper-point point-e" data-cropper-action="e"></span>' + 
+            '<span class="cropper-point point-n" data-cropper-action="n"></span>' + 
+            '<span class="cropper-point point-w" data-cropper-action="w"></span>' + 
+            '<span class="cropper-point point-s" data-cropper-action="s"></span>' + 
+            '<span class="cropper-point point-ne" data-cropper-action="ne"></span>' + 
+            '<span class="cropper-point point-nw" data-cropper-action="nw"></span>' + 
+            '<span class="cropper-point point-sw" data-cropper-action="sw"></span>' + 
+            '<span class="cropper-point point-se" data-cropper-action="se"></span>' + 
+      '</div>' + 
   '</div>';
 
   /**
@@ -3486,10 +3505,15 @@ console.log("In cp.js");
         var template = document.createElement('div');
         template.innerHTML = TEMPLATE;
         var cropper = template.querySelector(".".concat(NAMESPACE, "-container"));
+        console.log("Line 3508 : " + ".".concat(NAMESPACE, "-container"));
         var canvas = cropper.querySelector(".".concat(NAMESPACE, "-canvas"));
+        console.log("Line 3510 : " + ".".concat(NAMESPACE, "-canvas"));
         var dragBox = cropper.querySelector(".".concat(NAMESPACE, "-drag-box"));
+        console.log("Line 3512 : " + ".".concat(NAMESPACE, "-drag-box"));
         var cropBox = cropper.querySelector(".".concat(NAMESPACE, "-crop-box"));
+        console.log("Line 3514 : " + ".".concat(NAMESPACE, "-crop-box"));
         var face = cropBox.querySelector(".".concat(NAMESPACE, "-face"));
+        console.log("Line 3516 : " + ".".concat(NAMESPACE, "-face"));
         this.container = container;
         this.cropper = cropper;
         this.canvas = canvas;
