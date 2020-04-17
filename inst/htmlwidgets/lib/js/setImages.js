051ce13d9ce98d5f1ebeac4ab9833ec7518c0167
dmdeveloper <kudatawira@gmail.com>
2020-04-17 12:35:01 +0200

    update scripts

--- inst/htmlwidgets/lib/js/setImages.js
+++ inst/htmlwidgets/lib/js/setImages.js
@@ -1,12 +1,17 @@
 
 console.log("new setCanvass");
 function setCanvas(targetID,imgSrc){
-let imgId = 'currnt-img' + targetID.substring(14, targetID.length);
+let imgId = 'currnt-img_' + targetID.substring(14, targetID.length);
 if ( $('#'+targetID+' img' ).length ) {
      $('#'+targetID+' img' ).attr('src',imgSrc );
     //console.log("img exist : ");
   }else{
      $('#'+targetID).prepend($('<img>',{id:imgId,src:imgSrc,alt:'camtrap'}));
-     $("#"+imgId).imgViewer();
+
+     $(document).ready( function() {
+
+						$("#"+imgId).imgViewer());
+      });
+
   }
 }
