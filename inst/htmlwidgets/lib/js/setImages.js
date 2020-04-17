b54f753a005af49159c7bd18230fb5f4d968a933
dmdeveloper <kudatawira@gmail.com>
2020-04-17 14:14:03 +0200

    Update setImages.js

--- inst/htmlwidgets/lib/js/setImages.js
+++ inst/htmlwidgets/lib/js/setImages.js
@@ -1,5 +1,5 @@
 
-console.log("neww setCanvas");
+console.log("new setCanvas");
 function setCanvas(targetID,imgSrc){
 let imgId = 'currnt-img_' + targetID.substring(14, targetID.length);
 if ( $('#'+targetID+' img' ).length ) {
@@ -8,7 +8,7 @@ if ( $('#'+targetID+' img' ).length ) {
   }else{
 
 
-     $('#'+targetID).prepend($('<img>',{id:imgId,src:imgSrc,alt:'camtrap',height:'500px'}));
+     $('#'+targetID).prepend($('<img>',{id:imgId,src:imgSrc,alt:'camtrap',width:'100%',height:'500px'}));
 
 
       wheelzoom(document.querySelector("#"+imgId));
